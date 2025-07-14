9d2c4c641e485414c7c08d4215860568e5eb2f30
sajid shaikh <sajidsultan312@gmail.com>
2025-07-14 13:16:40 +0530

    final change

--- src/main.jsx
+++ src/main.jsx
@@ -1,51 +1,13 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { getCurrentUser } from "./api/api";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import Upload from "./pages/Upload";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 
-function App() {
-  const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
-
- const fetchUser = async () => {
-  try {
-    const res = await getCurrentUser();
-    setUser(res.data.user || null); 
-  } catch (err) {
-    if (err.response && err.response.status === 401) {
-      setUser(null);
-    } else {
-      console.error("Error fetching user:", err);
-    }
-  } finally {
-    setLoading(false);
-  }
-};
-
-
-
-  useEffect(() => {
-    fetchUser();
-  }, []);
-
-  if (loading) return <p>Loading...</p>;
-
-  return (
-    <>
-     <Navbar user={user} setUser={setUser} />
-      <Routes>
-        <Route path="/" element={<Home user={user} />} />
-        <Route
-          path="/upload"
-          element={
-            user ? <Upload user={user} /> : <Navigate to="/" replace />
-          }
-        />
-      </Routes>
-    </>
-  );
-}
-
-export default App;
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
